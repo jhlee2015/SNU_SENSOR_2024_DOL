bebd017731a7a136416ea122d9456ebea994db61
jhlee2015 <jhlee20110907@gmail.com>
2024-05-23 12:19:44 +0900

    db input

--- up_logger_manager.py
+++ up_logger_manager.py
@@ -23,7 +23,7 @@ class LoggerManager:
         self.console_handler.setFormatter(self.formatter)
 
         # self.info_log_init()
-        # self.serial_log_init()
+        self.serial_log_init()
         # self.db_log_init()
 
     def info_log_init(self):
