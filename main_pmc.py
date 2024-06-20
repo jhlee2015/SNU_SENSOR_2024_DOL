58dba32753a3fff15ec422a046b4597420211a7b
jhlee2015 <jhlee20110907@gmail.com>
2024-06-20 14:44:55 +0900

    pmc 코드 수정

--- main_pmc.py
+++ main_pmc.py
@@ -34,7 +34,7 @@ class PMC:
         while True:
             serial_logger.info("Pmc Control Request")
             ser.write(PMC.pmc_req)
-            time.sleep(6)
+            time.sleep(60)
 
     @staticmethod
     def pmc_parser(DATA):
