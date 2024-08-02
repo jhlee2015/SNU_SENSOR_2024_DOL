43306efd79864a2ced8c82fdd498dca6fd24b86c
jhlee2015 <jhlee20110907@gmail.com>
2024-08-02 15:19:07 +0900

    백업 관련

--- main_backup.py
+++ main_backup.py
@@ -7,6 +7,7 @@ import tkinter as tk
 from tkinter import filedialog
 
 import up_util
+import os
 
 # Connect to the database
 db_manager = up_databases.DatabaseManager()
@@ -34,12 +35,22 @@ if os_config.get('os_name') == 'WINDOW':
 elif os_config.get('os_name') == 'LINUX':
 
     # linux용
-    with open('log/sensing_value_old.csv', 'r') as file:
-        print('Reading CSV')
-        csv_reader = csv.reader(file)
-        print(csv_reader)
+    # Read the selected CSV file and insert its contents into the database
+    # Get the list of files in the "log" directory that start with "csv.log."
+    file_list = [file_name for file_name in os.listdir('log') if file_name.startswith('csv.log.')]
 
-        for row in csv_reader:
-            print('row' + str(row))
-            db_manager.backUpinsert(query=db_manager.insertQuery, params=(row[0], row[1], row[2], row[3]))
-        print('Finished reading CSV')
+    # Iterate over each file in the directory
+    for file_name in file_list:
+        file_path = os.path.join('log', file_name)
+        
+        # Read the selected CSV file and insert its contents into the database
+        with open(file_path, 'r') as file:
+            print('Reading CSV')
+            
+            csv_reader = csv.reader(file)
+            print(csv_reader)
+
+            for row in csv_reader:
+                print('row' + str(row))
+                db_manager.backUpinsert(query=db_manager.insertQuery, params=(row[0], row[1], row[2], row[3]))
+            print('Finished reading CSV')
