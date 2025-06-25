68f704d2dcdf1c899e7b2e7ea2d8e3cca2d15035
jhlee2015 <jhlee20110907@gmail.com>
2025-06-25 17:58:46 +0900

    modbus 형태로 추가

--- main_supmea_modbus.py
+++ main_supmea_modbus.py
@@ -8,54 +8,61 @@ import up_util
 import up_logger_manager
 import up_databases
 import up_config_manager
+from pymodbus.client.serial import ModbusSerialClient as ModbusClient
 
 # 한기술에서 사용하는 압력센서 supmea, SUP-PX400
 
 class SUPMEA:
 
-    kisan_req = bytearray([0x01, 0x04, 0x00, 0x82, 0x00, 0x08, 0x51, 0xE4])
+    #kisan_req = bytearray([0x01, 0x04, 0x00, 0x82, 0x00, 0x08, 0x51, 0xE4])
 
     def __init__(self):
-        serial_config = up_config_manager.ConfigManager().get_serial_config('AMA2')
+        serial_config = up_config_manager.ConfigManager().get_serial_config('TTY0')
         sensor_id = up_config_manager.ConfigManager().get_sensor_id()
         print(serial_config)
         print(sensor_id)
         self.port = serial_config['port']
         self.baud = serial_config['baud']
         self.sensor_id = sensor_id['id']
-        self.ser = None
+        self.client = None
         self.db = None
 
     def app_init(self):
-        self.ser = serial.Serial(self.port, self.baud, timeout=1)
+        self.client = ModbusClient(
+            port=self.port,
+            baudrate=self.baud,
+            parity='N',
+            stopbits=1,
+            bytesize=8,
+            timeout=1
+        )
+
 
     @staticmethod
-    def readthread(ser):  # 데이터 받는 함수
+    def readthread(client):  # 데이터 받는 함수
 
         while True:
-            serial_logger.info("Kisan Sensor Request!")
-            ser.write(SUPMEA.kisan_req)
-            time.sleep(60)
+            if client.connect():
+                slave_id = 1  # 슬레이브 ID
+                address = 130  # 레지스터 시작 주소
+                count = 8  # 읽을 레지스터 수
+                serial_logger.info("Kisan Sensor Request!")
+                read_result = client.read_input_registers(address=address, count=count, slave=slave_id)
 
-    @staticmethod
-    def kisan_parser(DATA):
-        try:
-            ret = util.hextodec(DATA, "input")
-            serial_logger.info(ret)
+                if read_result.isError():
+                    print("read fail:", read_result)
+                else:
+                    values = read_result.registers
+                    print(f"[read success] address {address}, Count{count} : {values}")
+                    cal_val = (values[0] / 65535.0) * 20.0  # [0] 0채널
+                    print(f"{cal_val:.3f} mA")
 
-            # NH3
-            nh3 = SUPMEA.NH3(DATA[3:5])
-            serial_logger.info("nh3 val : " + nh3)
-            return nh3
-        except Exception as E:
-            serial_logger.debug("parsing error" + str(E))
+                client.close()
+                time.sleep(1)
+            else:
+                print("Modbus connected fail")
 
-    @staticmethod
-    def NH3(data):
-        n = int(data.hex(), 16)
-        V = (n * 10) / 65535
-        nh3 = "{0:.2f}".format(V * 10)
-        return nh3
+
 
     def main_loof(self):
         while True:
@@ -88,13 +95,13 @@ if __name__ == '__main__':
         try:
             sup = SUPMEA()
             sup.app_init()
-            thread = threading.Thread(target=SUPMEA.readthread, args=(sup.ser,))  # 시리얼 통신 받는 부분
+            thread = threading.Thread(target=SUPMEA.readthread, args=(sup.client,))  # 시리얼 통신 받는 부분
             thread.start()
-            sup.main_loof()
+            # sup.main_loof()
 
         except Exception as E:
             serial_logger.info('main error' + str(E))
-            if sup.ser is not None:
+            if sup.client is not None:
                 serial_logger.info('serial close ok')
-                sup.ser.close()
+                sup.client.close()
             time.sleep(10)
