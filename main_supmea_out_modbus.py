02f8a6ecd365e41f6204127a8049d8b2d0a5bf18
jhlee2015 <jhlee20110907@gmail.com>
2025-06-26 21:07:23 +0900

    port 초기화

--- main_supmea_out_modbus.py
+++ main_supmea_out_modbus.py
@@ -34,10 +34,7 @@ class SUPMEA:
 
     def readthread(self):  # 데이터 받는 함수
         serial_logger.info("Kisan out ")
-        serial_config = up_config_manager.ConfigManager().get_serial_config('AMA4')
-        sensor_id = up_config_manager.ConfigManager().get_sensor_id()
-        print(serial_config)
-        print(sensor_id)
+        self.__init__()
         self.client = minimalmodbus.Instrument(self.port, slaveaddress=1, mode='rtu')
         self.client.serial.baudrate = int(self.baud)
         self.client.serial.bytesize = 8
